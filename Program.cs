7b15007af8fc4c7bf2bf7908dc4fb60751c0be74
Sandra Nilsen Brattli-Aspvik <sandrabrattliaspvik@gmail.com>
2026-08-25 20:46:16 +0200

    add calls to the new class methods in program.cs

--- Program.cs
+++ Program.cs
@@ -8,6 +8,10 @@ var stopWatch = new Stopwatch();
 int[] bubbleData = (int[])original.Clone();
 int[] quickSortData = (int[])original.Clone();
 int[] mergeSortData = (int[])original.Clone();
+int[] insertionSortData = (int[])original.Clone();
+int[] selectionSortData = (int[])original.Clone();
+int[] heapSortData = (int[])original.Clone();
+int[] radixSortData = (int[])original.Clone();
 
 // Bubblesort:
 stopWatch.Restart();
@@ -29,3 +33,31 @@ MergeSort.Sort(mergeSortData, 0, mergeSortData.Length -1);
 stopWatch.Stop();
 Console.WriteLine($"MergeSorted array: {string.Join(", ", mergeSortData)}");
 Console.WriteLine($"Time MergeSort: {stopWatch.ElapsedTicks} ticks\n");
+
+// InsertionSort:
+stopWatch.Restart();
+InsertionSort.Sort(insertionSortData);
+stopWatch.Stop();
+Console.WriteLine($"InsertionSorted array: {string.Join(", ", insertionSortData)}");
+Console.WriteLine($"Time InsertionSort: {stopWatch.ElapsedTicks} ticks\n");
+
+// SelectionSort
+stopWatch.Restart();
+SelectionSort.Sort(selectionSortData);
+stopWatch.Stop();
+Console.WriteLine($"SelectionSorted array: {string.Join(", ", selectionSortData)}");
+Console.WriteLine($"Time SelectionSort: {stopWatch.ElapsedTicks} ticks\n");
+
+// HeapSort:
+stopWatch.Restart();
+HeapSort.Sort(heapSortData);
+stopWatch.Stop();
+Console.WriteLine($"HeapSorted array: {string.Join(", ", heapSortData)}");
+Console.WriteLine($"Time HeapSort: {stopWatch.ElapsedTicks} ticks\n");
+
+// RadixSort:
+stopWatch.Restart();
+RadixSort.Sort(radixSortData);
+stopWatch.Stop();
+Console.WriteLine($"RadixSorted array: {string.Join(", ", radixSortData)}");
+Console.WriteLine($"Time RadixSort: {stopWatch.ElapsedTicks} ticks\n");
