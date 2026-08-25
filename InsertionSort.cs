b69d7f88040a4e4313491b45845fdcf18aa701f8
Sandra Nilsen Brattli-Aspvik <sandrabrattliaspvik@gmail.com>
2026-08-25 20:45:16 +0200

    add InsertionSort.cs

--- InsertionSort.cs
+++ InsertionSort.cs
@@ -1,6 +1,20 @@
 ﻿namespace SortingAlgorithmPractice;
 
-public class InsertionSort
+public static class InsertionSort
 {
-    
+    public static void Sort(int[] array)
+    {
+        for (int i = 1; i < array.Length; i++)
+        {
+            int key = array[i];
+            int j = i - 1;
+
+            while (j >= 0 && array[j] > key)
+            {
+                array[j + 1] = array[j];
+                j--;
+            }
+            array[j + 1] = key;
+        }
+    }
 }
