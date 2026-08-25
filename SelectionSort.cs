1a04aaaa7ddc79f8de1744be7f771268075ba756
Sandra Nilsen Brattli-Aspvik <sandrabrattliaspvik@gmail.com>
2026-08-25 20:45:54 +0200

    add selectionsort

--- SelectionSort.cs
+++ SelectionSort.cs
@@ -2,5 +2,20 @@
 
 public class SelectionSort
 {
-    
+    public static void Sort(int[] arr)
+    {
+        for (int i = 0; i < arr.Length - 1; i++)
+        {
+            int minIndex = i;
+
+            for (int j = i + 1; j < arr.Length; j++)
+            {
+                if (arr[j] < arr[minIndex])
+                    minIndex = j;
+            }
+
+            if (minIndex != i)
+                (arr[i], arr[minIndex]) = (arr[minIndex], arr[i]);
+        }
+    }
 }
