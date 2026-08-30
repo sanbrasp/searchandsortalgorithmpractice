572d51293f718a8a09d5062ff1d2c6210f748839
Sandra Nilsen Brattli-Aspvik <sandrabrattliaspvik@gmail.com>
2026-08-30 20:43:37 +0200

    fix reverted .csproj file to allow access for test project. re-add logic for LinearSearch after some git nonsense happened

---
+++ SearchAlgorithmPractice/Helpers/SearchHelpers.cs
@@ -0,0 +1,28 @@
+﻿using SearchAlgorithmPractice.Models;
+
+namespace SearchAlgorithmPractice.Helpers;
+
+internal static class SearchHelpers
+{
+    internal static SearchResult<T> LinearSearch<T>(T target, IEnumerable<T> array)
+    {
+        var comparer = EqualityComparer<T>.Default;
+        int index = 0;
+
+        foreach (var item in array)
+        {
+            if (comparer.Equals(item, target))
+            {
+                return new SearchResult<T>(true, index, item);
+            }
+            index++;
+        }
+
+        return new SearchResult<T>(false, -1, default);
+    }
+
+    internal static SearchResult<T> BinarySearch<T>(T target, IReadOnlyList<T> array)
+    {
+        throw new NotImplementedException();
+    }
+}
