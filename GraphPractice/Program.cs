57ed5369043c555a8f081ba81540b1e817925f4c
Sandra Nilsen Brattli-Aspvik <sandrabrattliaspvik@gmail.com>
2026-09-01 17:06:14 +0200

    graph testing in Program.cs

--- GraphPractice/Program.cs
+++ GraphPractice/Program.cs
@@ -1,9 +1,18 @@
-﻿namespace GraphPractice;
+﻿using GraphPractice.Models;
+
+namespace GraphPractice;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        var graph = new Graph<string>();
+        graph.AddEdge("A", "B");
+        graph.AddEdge("A", "C");
+        graph.AddEdge("B", "D");
+
+        Console.WriteLine(string.Join(", ", graph.GetNeighbors("A"))); // Returns B and C
+        Console.WriteLine(graph.ContainsNode("D")); // Returns true
+        Console.WriteLine(graph.ContainsNode("Z")); // Returns false
     }
 }
