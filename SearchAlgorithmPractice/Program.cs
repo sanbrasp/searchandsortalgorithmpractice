ae0828f0fcaf7403bb4d9e0e34dd8aeb121a73fa
Sandra Nilsen Brattli-Aspvik <sandrabrattliaspvik@gmail.com>
2026-09-02 20:08:14 +0200

    test QueueExample.

--- SearchAlgorithmPractice/Program.cs
+++ SearchAlgorithmPractice/Program.cs
@@ -1,1 +1,5 @@
-﻿Console.WriteLine("Hello, World! Sandra added nothing useful to this Program.cs!");
+﻿using SearchAlgorithmPractice;
+
+Console.WriteLine("Hello, World! Sandra added nothing useful to this Program.cs!");
+
+QueueExample.CountNumbers();
